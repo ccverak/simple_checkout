d2a98dd03863840279b7a09b3a9cd2ece897eaa5
Carlos Castellanos Vera <ccverak@gmail.com>
2019-07-03 13:26:26 +0200

    Fixes in logging

--- app/api/defaults.rb
+++ app/api/defaults.rb
@@ -10,13 +10,13 @@ module CabWeb
           content_type :json, "application/json"
 
           rescue_from Grape::Exceptions::ValidationErrors do |e|
-            logger.error(e) unless ENV["RACK_ENV"] == "test"
+            Grape::API.logger.error(e) unless ENV["RACK_ENV"] == "test"
 
             error!(e, 400)
           end
 
           rescue_from Cab::Errors::RecordNotFound do |e|
-            logger.error(e) unless ENV["RACK_ENV"] == "test"
+            Grape::API.logger.error(e) unless ENV["RACK_ENV"] == "test"
 
             error!(e, 404)
           end
@@ -25,7 +25,7 @@ module CabWeb
             unless ENV["RACK_ENV"] == "test"
               Rollbar.error(e)
 
-              logger.error(e)
+              Grape::API.logger.error(e)
             end
             error!(message: "Internal server error", status: 500)
           end
